bcff4de66dfbaa77eab873c47a7fb81d0696a1f5
aallen170 <aallen170@gmail.com>
2019-08-14 08:53:28 -0500

    update

---
+++ function.js
@@ -0,0 +1,42 @@
+let num1 = 1, num2 = 2, num3 = 3;
+
+function func1(a, b, c, d) {
+    let numA;
+
+    num1 = a;
+    num2 = b;
+    num3 = c;
+
+    numA = d;
+
+    let z = 222;
+
+    console.log(num1);
+    console.log(num2);
+    console.log(num3);
+
+    return z;
+}
+
+// let i = 0;
+
+// while (i < 5) {
+//     console.log(`before increment ${i}`);
+//     i++;
+// }
+
+let l = 4, k = `1`;
+
+console.log(`number = ${l} and k = ${k}`);
+// console.log("number = " + l + " and k = " + k);
+
+// if (l === k) console.log("yes");
+// else console.log("no");
+
+// for (let i = 0; i <= 6; i += 2) {
+//     console.log(i);
+// }
+
+// let numB = func1(10, 20, 30, 60);
+
+// console.log(numB);
---
+++ notes.js
@@ -0,0 +1,10 @@
+// && AND operator = true ONLY when both booleans 
+// are true
+// || OR operator = false ONLY when both booleans 
+// are false
+// ! operator = reverse boolean statement
+// 0 = false; 1 = true
+
+// VS Code: Ctrl + d to pick next instance of highlighted text
+
+// bash: 'grep' searches for a keyword from a text input
