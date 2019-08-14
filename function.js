20e06edf34510dbf84bf9434d30ed111479b15ab
PlasmaMyke <mychala@sbcglobal.net>
2019-08-14 09:56:21 -0400

    second commit

---
+++ function.js
@@ -0,0 +1,15 @@
+let num1 = 1, num2 = 2, num3 = 3
+
+function func1(a, b, c){
+    num1 = a;
+    num2 = b;
+    num3 = c;
+
+    console.log(num1);
+    console.log(num2);
+    console.log(num3);
+}
+
+func1(10, 20, 30)
+
+//
